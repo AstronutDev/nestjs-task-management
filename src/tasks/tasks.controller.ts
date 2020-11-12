dd663858b1ad95ebc5dbdb0e366d9c92106f431e
Supanut Laddayam <ynotmela@gmail.com>
2020-11-12 13:28:11 +0700

    add validator to search&filter

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -10,7 +10,7 @@ export class TasksController {
     constructor(private taskService: TasksService) {}
 
     @Get()
-    getTasks(@Query() filterDto: GetTaskFilterDto): Task[] {
+    getTasks(@Query(ValidationPipe) filterDto: GetTaskFilterDto): Task[] {
         if(Object.keys(filterDto).length) {
             return this.taskService.getTaskWithFilter(filterDto)
         } else {
