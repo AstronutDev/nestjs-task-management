13818f3c96b393982245e4725c56a4f21a6eedf7
Supanut Laddayam <ynotmela@gmail.com>
2020-11-13 14:58:02 +0700

    add errorHandling, duplicate username

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,3 +1,4 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm";
 import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { User } from "./user.entity";
@@ -6,11 +7,19 @@ import { User } from "./user.entity";
 export class UserRepository extends Repository<User> {
     async signUp(authCredentialDto: AuthCredentialsDto): Promise<void> {
         let {username, password} = authCredentialDto
-
         let user = new User()
         user.username = username
         user.password = password
 
-        await user.save()
+        try {
+            await user.save()
+        } catch (error) {
+            if(error.code === '23505') {
+                throw new ConflictException('Username already exists')
+            } else {
+                throw new InternalServerErrorException()
+            }
+               
+        }
     }
 }
